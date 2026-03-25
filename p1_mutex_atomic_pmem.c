bde16c99245c45398d7d3d8c1150453b58226517
AxeKam333 <olek.kaminski5@gmail.com>
2026-03-25 10:35:11 +0100

    Add pmem warmup stage

--- p1_mutex_atomic_pmem.c
+++ p1_mutex_atomic_pmem.c
@@ -41,8 +41,13 @@ int main() {
     printf("Test przepustowosci...\n");
     printf("Liczba wątków, Czas wykonania [s]: \n");
 
-    for(int j = 1; j <= 2; j++) {
-        for(int i = 0; i < l_krokow; i++) {
+    for(int i = 0; i < l_krokow; i++) {
+        int w = krok_test[i];
+            z_czas(w);
+    }
+
+    for(int i = 0; i < l_krokow; i++) {
+        for(int j = 1; j <= 6; j++) {
             int w = krok_test[i];
             printf("%d, %f\n", w, z_czas(w));
         }
