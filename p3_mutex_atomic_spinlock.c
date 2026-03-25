d711f3dcf01896bc2487f30a994bf1dff56c9861
AxeKam333 <olek.kaminski5@gmail.com>
2026-03-25 14:36:08 +0100

    Add spinlock. Delete old programs

--- p3_mutex_atomic_spinlock.c
+++ p3_mutex_atomic_spinlock.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <time.h>
-#include <stdatomic.h>
 #include <stdlib.h>
 
 #define TEST_SIZE 6
-#define ITER 100000000
+#define ITER 1000000
 
 static pthread_barrier_t barrier;
-volatile atomic_int ctr;
-// pthread_mutex_t L = PTHREAD_MUTEX_INITIALIZER;
+static pthread_spinlock_t spinlock;
+static int ctr;
 
 void* increment(void* arg) {
-    long int * liczba_watkow = (long int *)arg;
+    long int *liczba_watkow = (long int *)arg;
     pthread_barrier_wait(&barrier);
-
-    for(long int i = 0; i < ITER/(*liczba_watkow); i++) {
+    for(long int i = 0; i < ITER / (*liczba_watkow); i++) {
+        pthread_spin_lock(&spinlock);
         ctr++;
+        pthread_spin_unlock(&spinlock);
     }
     return NULL;
 }
@@ -24,13 +24,11 @@ void* increment(void* arg) {
 double z_czas(long int liczba_watkow) {
     pthread_t watki[96];
     struct timespec start, end;
-    
-    // Reset licznika przed każdym testem
-    ctr = 0; 
-    
-    int binit = pthread_barrier_init(&barrier, NULL, liczba_watkow + 1);
-    if (binit != 0)
-        perror("pthread_barrier_init");
+
+    ctr = 0;
+
+    pthread_barrier_init(&barrier, NULL, liczba_watkow + 1);
+    pthread_spin_init(&spinlock, PTHREAD_PROCESS_PRIVATE);
 
     for(long int i = 0; i < liczba_watkow; i++) {
         pthread_create(&watki[i], NULL, increment, &liczba_watkow);
@@ -38,37 +36,26 @@ double z_czas(long int liczba_watkow) {
 
     pthread_barrier_wait(&barrier);
     clock_gettime(CLOCK_MONOTONIC, &start);
-
     for(long int i = 0; i < liczba_watkow; i++) {
         pthread_join(watki[i], NULL);
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
 
     pthread_barrier_destroy(&barrier);
-    
-    // long int iterations = (ITER / liczba_watkow) * liczba_watkow;
-    double time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / (1e9);
-    return (ITER)/time;
+    pthread_spin_destroy(&spinlock);
+
+    double time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    return ITER / time;
 }
 
 int main() {
     int krok_test[] = {1, 2, 4, 8, 16, 24, 32, 48, 64, 80, 96};
     int l_krokow = sizeof(krok_test) / sizeof(krok_test[0]);
-    printf("Test przepustowosci...\n");
-    printf("Liczba wątków, Przepustowość [ops/s]: \n");
-    
-    // rozgrzewka
-    for(int i = 0; i < l_krokow; i++) {
-        int w = krok_test[i];
-        z_czas(w);
-    }
 
-    for(int i = 0; i < l_krokow; i++) {
-        for(int j = 1; j <= TEST_SIZE; j++) {
-            int w = krok_test[i];
-            printf("%d, %f\n", w, z_czas(w));
-        }
-    }
+    printf("Liczba wątków, Przepustowość [ops/s]:\n");
+
+    for(int i = 0; i < l_krokow; i++)
+        z_czas(krok_test[i]);  // rozgrzewka
 
     for(int w = 1; w <= 32; w++) {
         for(int j = 1; j <= TEST_SIZE; j++) {
@@ -76,5 +63,10 @@ int main() {
         }
     }
 
+    for(int i = 0; i < l_krokow; i++) {
+        for(int j = 0; j < TEST_SIZE; j++) {
+            printf("%d, %f\n", krok_test[i], z_czas(krok_test[i]));
+        }
+    }
     return 0;
 }
