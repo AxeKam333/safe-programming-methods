d5eb2821d8302ef0ecb789f5158647a73cc61cdc
AxeKam333 <olek.kaminski5@gmail.com>
2026-03-30 19:24:15 +0200

    Add ARM64 architecture

--- p2_double_check/MBP_DCL_1/obj/Debug/net8.0/MBP_DCL_1.AssemblyInfo.cs
+++ p2_double_check/MBP_DCL_1/obj/Debug/net8.0/MBP_DCL_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBP_DCL_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189dcbe8bfcc6bfdd6335bcf2fb353b01867f2a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBP_DCL_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBP_DCL_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
