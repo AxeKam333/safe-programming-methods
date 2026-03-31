b58507724e45cec5d3fbfda2946d2059842c3cb1
MattiCoder <119958591+MattiCoder@users.noreply.github.com>
2026-03-31 14:43:18 +0200

    Add files via upload
    wersja podstawowa do testowania - 1 mln prób, błędna wersja z dwoma ifami.

---
+++ p2_double_check/mbp_doublecheck_v_1/mbp_doublecheck_v_1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DclTest
+{
+    public class Data
+    {
+        public List<int> Lista;
+
+        public Data()
+        {
+            Lista = new List<int>();
+            for (int i = 0; i < 1000; i++) Lista.Add(i);
+        }
+    }
+
+    public class Singleton
+    {
+        private static Data _instance;
+        private static readonly object _lock = new object();
+        public static void Reset() => _instance = null;
+
+        public static Data GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Data();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int threadCount = Environment.ProcessorCount;
+            Console.WriteLine($"Start testu DCL na {threadCount} wątkach...");
+
+            bool error = false;
+            long attempts = 1000000;
+
+            for (long i = 0; i < attempts; i++)
+            {
+                Singleton.Reset();
+                Barrier barrier = new Barrier(threadCount);
+                Task[] tasks = new Task[threadCount];
+
+                for (int t = 0; t < threadCount; t++)
+                {
+                    tasks[t] = Task.Run(() =>
+                    {
+                        barrier.SignalAndWait();
+
+                        Data data = Singleton.GetInstance();
+
+                        try
+                        {
+                            if (data.Lista == null || data.Lista.Count < 1000)
+                                error = true;
+                        }
+                        catch
+                        {
+                            error = true;
+                        }
+                    });
+                }
+
+                Task.WaitAll(tasks);
+
+                if (error)
+                {
+                    Console.WriteLine($"\nBŁĄD Otrzymano niedokończony obiekt w próbie nr {i}!");
+                    break;
+                }
+            }
+
+            if (!error)
+            {
+                Console.WriteLine("\nWykonano 1 milion prób. Nie wywołano błędu.");
+            }
+        }
+    }
+}
